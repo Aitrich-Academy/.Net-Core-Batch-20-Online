a066cca52594e89b75c08226e1b29186594fdb19
RadhiKrishna <radhikrishna2@gmail.com>
2025-06-17 11:27:13 +0530

    ticket_systm

---
+++ EFCore/Radhi/TICKET-Booking/Models/BookingDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TICKET_Booking.Models
+{
+    public class BookingDbContext:DbContext
+    {
+     public DbSet<Booking> bookings {  get; set; }
+       public  DbSet<Bus> bus{  get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Data Source=DESKTOP-3OCUFDV;Initial Catalog=Ticket;Integrated Security=True;Trust Server Certificate=True");
+        }
+
+    }
+}
