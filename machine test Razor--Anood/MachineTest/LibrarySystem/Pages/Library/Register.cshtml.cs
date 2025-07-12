883fe42e9d699bd7ccd0ffb4172c968284077aee
anoodnazeer <anoodnazeer@gmail.com>
2025-07-12 20:21:28 +0400

    add machine test razor

---
+++ machine test Razor--Anood/MachineTest/LibrarySystem/Pages/Library/Register.cshtml.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using LibrarySystem.Model;
+using static LibrarySystem.Model.Role;
+
+namespace LibrarySystem.Pages.Library
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RegisterModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public User NewUser { get; set; }
+
+        [BindProperty]
+        public Role SelectedRole { get; set; }
+        public IActionResult OnPost()
+        {
+            if (string.IsNullOrEmpty(NewUser.Username) || string.IsNullOrEmpty(NewUser.Password))
+            {
+                ModelState.AddModelError("", "Username and Password are required");
+                return Page();
+            }
+
+            // Check if user already exists
+            if (_context.Users.Any(u => u.Username == NewUser.Username))
+            {
+                ModelState.AddModelError("", "Username already taken");
+                return Page();
+            }
+
+            _context.Users.Add(NewUser);
+            _context.SaveChanges();
+            return RedirectToPage("/MyUser/Login");
+        }
+    }
+}
