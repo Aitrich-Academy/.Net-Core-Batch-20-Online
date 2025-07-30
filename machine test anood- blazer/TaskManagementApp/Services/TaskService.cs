37881bf9c92f9883a9fa5da7a67dc8178bcac3e5
anoodnazeer <anoodnazeer@gmail.com>
2025-07-30 22:31:39 +0400

    add machine test blazer

---
+++ machine test anood- blazer/TaskManagementApp/Services/TaskService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TaskManagementApp.Interfaces;
+using TaskManagementApp.Models;
+using TaskManagementApp.Repositories;
+
+namespace TaskManagementApp.Services
+{
+    public class TaskService : ITaskService
+    {
+        private readonly TaskRepository _repository;
+
+        public TaskService(TaskRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<TaskItem>> GetAllTasksAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<TaskItem> GetTaskByIdAsync(int id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+
+        public async Task AddTaskAsync(TaskItem task)
+        {
+            if (task == null)
+                throw new ArgumentNullException(nameof(task));
+
+            await _repository.AddAsync(task);
+        }
+
+        public async Task UpdateTaskAsync(TaskItem task)
+        {
+            if (task == null)
+                throw new ArgumentNullException(nameof(task));
+
+            await _repository.UpdateAsync(task);
+        }
+
+        public async Task DeleteTaskAsync(int id)
+        {
+            await _repository.DeleteAsync(id);
+        }
+    }
+}
